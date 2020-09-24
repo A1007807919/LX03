6e6e732839fd18928205a3e5aefb5b10b4daa9c5
A1007807919 <71052820+A1007807919@users.noreply.github.com>
2020-09-24 11:46:26 +0800

    Add files via upload

---
+++ Result.cs
@@ -0,0 +1,34 @@
+﻿using MySqlX.XDevAPI.Common;
+using Org.BouncyCastle.Asn1.Crmf;
+using Remotion.Linq.Parsing.ExpressionVisitors.MemberBindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class Result<T>
+    {
+        public int Code = 1;
+        public string msg = "";
+        public T res = default(T);
+        public static Result Ok(T result)
+        {
+            return new Result() { res = result };
+        }
+        public static Result Ok(string messgge,T Result=default(T))
+        {
+            return new Result() { msg = messgge, res = Result };
+        }
+        public static Result Err (string messgge)
+        {
+            return new Result() { Code = -1, msg = messgge };
+        }
+    }
+    public class Result: Result<dynamic>
+    {
+
+    }
+}
---
+++ UsersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Json (DAL.UserInfo.Instance.GetCount());
+        }
+        [HttpPost("{username}")]
+        public ActionResult  post([FromBody]Model.UserInfo users)
+        {
+            try
+            {
+                int n = DAL.UserInfo.Instance.Add(users);
+                return Json(Result.Ok("添加成功"));
+            }
+            catch(Exception ex)
+            {
+                if (ex.Message.ToLower().Contains("primary"))
+                    return Json(Result.Err("用户名已存在"));
+                else if (ex.Message.ToLower().Contains("null"))
+                    return Json(Result.Err("用户名、密码、身份证不能为空"));
+                else
+                    return Json(Result.Err(ex.Message));
+            }
+        }
+    }
+}
